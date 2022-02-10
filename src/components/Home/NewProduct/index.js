b9304341354aedd2fc64b717c8811b56eca50c08
nataliel2514 <natalielopez2514@gmail.com>
2022-02-10 11:24:40 -0600

    html added for home page, home link added

---
+++ src/components/Home/NewProduct/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+function NewProduct() {
+    return (
+        <div className="newprod-container">
+            <h1>NEW PRODUCT</h1>
+            <p>XX99 MARK II HEADPHONES</p>
+            <p>lorem ipsum dolor sit amet</p>
+            <button>See Product</button>
+        </div>
+    )
+}
+
+export default NewProduct;
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div>
+            <Link to='/'>
+                Home
+            </Link>
             <Link to='/earphones'>
                 Earphones
             </Link>
