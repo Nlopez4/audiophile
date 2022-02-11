a5f1d15f8ef16765569147d1709a0115969c30ea
nataliel2514 <natalielopez2514@gmail.com>
2022-02-10 19:05:26 -0600

    products section added to homepage

---
+++ src/components/Home/Products/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css';
+
+function Products() {
+    return (
+        <section className="products-container">
+            <div className="product-1">
+                <h1>ZX9 SPEAKER</h1>
+                <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                <button>See Product</button>
+            </div>
+                <div className="product-2">
+                    <h1>ZX7 SPEAKER</h1>
+                    <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                    <button>See Product</button>
+                </div>
+        </section>
+    )
+}
+
+export default Products; 
