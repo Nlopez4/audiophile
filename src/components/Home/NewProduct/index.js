725126fe73eff1d4794c0e2d16a47bf6d44adee5
nataliel2514 <natalielopez2514@gmail.com>
2022-02-12 21:32:14 -0600

    add links for two speakers on home page

--- src/components/Home/NewProduct/index.js
+++ src/components/Home/NewProduct/index.js
@@ -3,15 +3,10 @@ import './styles.scss';
 import { useNavigate } from "react-router-dom";
 
 function NewProduct() {
-    // here is where we can logic, like props
-    // Need to make button work to take us to product page 
-
        const navigate = useNavigate(); 
        const seeNewProduct = () => {
            navigate('/NewProdItem')
        }
-
-
     return (
         <div className="newprod-container">
             <h1>NEW PRODUCT</h1>
@@ -20,8 +15,6 @@ function NewProduct() {
             <button onClick={() => navigate("/NewProdItem")}>See Product</button>
         </div>
     )
-
-    
 }
 
 export default NewProduct;
