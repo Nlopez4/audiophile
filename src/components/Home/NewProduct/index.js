0e1d23007df1206abff3c4da0b8d9116ce60d072
nataliel2514 <natalielopez2514@gmail.com>
2022-02-14 13:25:32 -0600

    add image in new prod component

--- src/components/Home/NewProduct/index.js
+++ src/components/Home/NewProduct/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './styles.scss';
 import { useNavigate } from "react-router-dom";
+import NewHeadphones from '../../../images/image-hero.jpg';
 
 
 function NewProduct() {
-       const navigate = useNavigate(); 
-       const seeNewProduct = () => {
-           navigate('/NewProdItem')
-       }
+    const navigate = useNavigate(); 
     return (
         <section className="newprod-container">
             <div className="newprod-info">
+                <img src={NewHeadphones} alt="" />
                 <h1>NEW PRODUCT</h1>
                 <p>XX99 MARK II HEADPHONES</p>
                 <p>lorem ipsum dolor sit amet</p>
