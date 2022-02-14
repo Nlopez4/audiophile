ad6ffd69805b92820ca13c6e64b50d4ff69b4b94
nataliel2514 <natalielopez2514@gmail.com>
2022-02-13 18:39:25 -0600

    add new background image to newprod componenent

--- src/components/Home/NewProduct/index.js
+++ src/components/Home/NewProduct/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.scss';
 import { useNavigate } from "react-router-dom";
 
+
 function NewProduct() {
        const navigate = useNavigate(); 
        const seeNewProduct = () => {
@@ -9,10 +10,12 @@ function NewProduct() {
        }
     return (
         <section className="newprod-container">
+            <div className="newprod-info">
             <h1>NEW PRODUCT</h1>
             <p>XX99 MARK II HEADPHONES</p>
             <p>lorem ipsum dolor sit amet</p>
             <button onClick={() => navigate("/NewProdItem")}>See Product</button>
+            </div>
         </section>
     )
 }
