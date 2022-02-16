c181d919cd8f5780bfe1338a40ccef81d32acf0e
nataliel2514 <natalielopez2514@gmail.com>
2022-02-15 20:14:13 -0600

    fix background, center text

--- src/components/Home/NewProduct/index.js
+++ src/components/Home/NewProduct/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './styles.scss';
 import { useNavigate } from "react-router-dom";
-import NewHeadphones from '../../../images/image-hero.jpg';
+// import NewHeadphones from '../../../images/image-hero.jpg';
 
 
 function NewProduct() {
     const navigate = useNavigate(); 
     return (
-        <section className="newprod-container" style={{ backgroundImage: `url(${NewHeadphones})` }}>
-            <div className="newprod-info">
-                <img src={NewHeadphones} alt="" />
-                <h1>NEW PRODUCT</h1>
-                <p>XX99 MARK II HEADPHONES</p>
-                <p>lorem ipsum dolor sit amet</p>
-                <button onClick={() => navigate("/NewProdItem")}>See Product</button>
+        <section className="newprod-container">
+            <div className="newprod-image">
+                <div className="newprod-info">
+                    <h1>NEW PRODUCT</h1>
+                    <p>XX99 MARK II HEADPHONES</p>
+                    <p>lorem ipsum dolor sit amet</p>
+                    <button onClick={() => navigate("/NewProdItem")}>See Product</button>
+                </div>
             </div>
         </section>
     )
 }
 
 export default NewProduct;
+
+// style={{ backgroundImage: `url(${NewHeadphones})` }}
