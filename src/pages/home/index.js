508ea83f1e1360c41eb9c1f3f827be435f660403
nataliel2514 <natalielopez2514@gmail.com>
2022-02-19 13:01:10 -0600

    delete products from homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './styles.scss';
 import NewProduct from '../../components/Home/NewProduct/index';
-import Links from '../../components/Home/Links/index';
-import Products from '../../components/Home/Products/index';
 import About from '../../components/Home/About/index';
 
 function HomePage() {
     return (
         <div className="home-container">
            <NewProduct />
-           <Links />
-           <Products />
            <About />
         </div>
     )
