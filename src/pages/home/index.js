c570eaf3cfa66a9387e3f2809685dcf056132110
nataliel2514 <natalielopez2514@gmail.com>
2022-02-20 11:39:06 -0600

    add ecommerce js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,7 @@ import './styles.scss';
 import NewProduct from '../../components/Home/NewProduct/index';
 import HeadPhones from '../../components/Home/HeadPhones/index';
 import About from '../../components/Home/About/index';
+import { commerce } from '../../lib/commerce';
 
 function HomePage() {
     return (
