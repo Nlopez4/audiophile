5e9b99b409ed1c3c66c73dd0913437ccaeb4629e
nataliel2514 <natalielopez2514@gmail.com>
2022-02-25 21:46:50 -0600

    add images to array object

--- src/components/Home/HeadPhones/index.js
+++ src/components/Home/HeadPhones/index.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import './styles.scss';
+import xx59 from '../../../images/xx59.jpg';
+import mark1 from '../../../images/mark1.jpg';
 
 // Display data here
 // Parent component 
 const products = [
-    { id: 1, name: 'XX99 Mark I Headphones', description: 'As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.', price:'$1,750'},
-    // { id: 2, name: 'XX59 Headphones', description: 'Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.', price:'$1,950'},
+    { id: 1, image: mark1, name: 'XX99 Mark I Headphones', description: 'As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.', price:'$1,750'},
+    { id: 2, image: xx59, name: 'XX59 Headphones', description: 'Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.', price:'$1,950'},
 ]
 
 
 function HeadPhones(props) {
     return (
         <section className="headphones-section">
-            <div className="mark1-image"></div>
-            {products.map((product) => (
-            <div className="headphones-info" key={product.id}>
-                <h1 className="prod-1">{product.name}</h1>
-                <p className="p-1">{product.description}</p>
-                <p className="price">{product.price}</p>
-                <button className="cart-btn">Add to Cart</button>
-            </div>
-            ))}
+            <div className="hp-container">
+                {products.map((product) => (
+                <div className="headphones-info" key={product.id}>
+                    <img className="product-images" src={product.image} />
+                    <h1 className="prod-1">{product.name}</h1>
+                    <p className="p-1">{product.description}</p>
+                    <p className="price">{product.price}</p>
+                    <button className="cart-btn">Add to Cart</button>
+                </div>
+                ))}
+             </div>
         </section>
     )
 }
