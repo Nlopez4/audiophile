527f5975d9ed23c527edc15d5b487ae59b02c375
nataliel2514 <natalielopez2514@gmail.com>
2022-03-09 20:45:01 -0600

    total added to cartlist

--- src/components/CartList/index.js
+++ src/components/CartList/index.js
@@ -6,16 +6,24 @@ import { CartContext } from '../../context';
 function CartList() {
     const { cart } = useContext(CartContext); 
     console.log(cart)
+    if (cart &&  cart.total_unique_items > 0) {
+        return (
+            <section className="cart-section">
+                <div className="cart-container">
+                {cart.line_items?.map(line_item => (
+                        <CartItem key={line_item.id} lineItem={line_item}/>
+                ))}
+                <p>Total: {cart.subtotal.formatted_with_symbol}</p>
+                <button>CHECK OUT</button>
+                </div>
+            </section>
+        )
+    }
     return (
-        <section className="cart-section">
-            <div className="cart-container">
-               {cart.line_items?.map(line_item => (
-                    <CartItem key={line_item.id} lineItem={line_item}/>
-               ))}
-
-            </div>
-        </section>
+        <div>
+            <p>Your cart is empty.</p>
+        </div>
     )
-}
+};
 
 export default CartList; 
