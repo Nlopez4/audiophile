fa64f073d3a15c858654ec8a6b6e286ef16f5ca3
nataliel2514 <natalielopez2514@gmail.com>
2022-03-12 17:09:35 -0600

    change button to min-width

--- src/components/CartList/index.js
+++ src/components/CartList/index.js
@@ -12,14 +12,14 @@ function CartList({lineItem}) {
             <section className="cart-section">
                 <div className="cart-container">
                     <div className="top-container">
-                        <p className="total-items-p">Total Items: {cart.total_items}</p>
+                        <p className="total-items-p">Cart ({cart.total_items})</p>
                         <button className="empty-btn" onClick={() => handleEmptyCart()}>EMPTY CART</button>
                     </div>
                     {cart.line_items?.map(line_item => (
                             <CartItem key={line_item.id} lineItem={line_item}/>
                     ))}
                     <div className="totals-container">
-                        <p className="subtotal-p">Total: {cart.subtotal.formatted_with_symbol}</p>
+                        <p className="subtotal-p">Total:  {cart.subtotal.formatted_with_symbol}</p>
                         <button className="check-out-btn">CHECK OUT</button>
                     </div>
                 </div>
