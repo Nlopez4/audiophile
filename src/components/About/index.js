9a4370a31c8a4714e58f65fbb9b7899615c41835
nataliel2514 <natalielopez2514@gmail.com>
2022-03-12 21:04:56 -0600

    basic responsive design added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,12 +5,14 @@ import AboutImg from '../../images/image-best-gear.jpg';
 function About() {
     return (
         <section className="about-section">
-            <div className="about-info">
-                <h1 className="about-h1">Bringing you the <span>best</span> audio gear </h1>
-                <p className="about-p">Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
-            </div>
-            <div className="about-img-container">
-                <img src={AboutImg} alt ="Individual with headphones" className="about-img"/>
+            <div className="about-container">
+                <div className="about-info">
+                    <h1 className="about-h1">Bringing you the <span>best</span> audio gear </h1>
+                    <p className="about-p">Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
+                </div>
+                <div className="about-img-container">
+                    <img className="about-img" src={AboutImg} alt ="Individual with headphones" className="about-img"/>
+                </div>
             </div>
         </section>
     )
