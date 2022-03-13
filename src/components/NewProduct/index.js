a093e8e082fa5b22620be1a506b10f4c23133d2f
nataliel2514 <natalielopez2514@gmail.com>
2022-03-12 19:53:23 -0600

    fix scss for new prod section

--- src/components/NewProduct/index.js
+++ src/components/NewProduct/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './styles.scss';
-import { useNavigate } from "react-router-dom";
 
 
 function NewProduct() {
-    const navigate = useNavigate(); 
     return (
         <section className="newprod-section">
-            <div className="newprod-image">
+            <div className="newprod-container">
                 <div className="newprod-info">
                     <h1 className="new-text">NEW PRODUCT</h1>
                     <p className="newprod-name">XX99 MARK II HEADPHONES</p>
                     <p className="newprod-info-text">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-                    <button className="newprod-bttn" onClick={() => navigate("/NewProdItem")}>See Product</button>
                 </div>
-            </div>
+                <div className="newprod-image"></div>
+                </div>
         </section>
     )
 }
