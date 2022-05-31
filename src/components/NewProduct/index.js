19bd6bafbc816ca4868779e88cbfe0fa144d7acb
nataliel2514 <natalielopez2514@gmail.com>
2022-05-30 19:04:53 -0500

    add px on max width for desktop

--- src/components/NewProduct/index.js
+++ src/components/NewProduct/index.js
@@ -10,7 +10,7 @@ function NewProduct() {
                     <div class="overlay">
                         <h1 className="new-text">NEW PRODUCT</h1>
                         <p className="newprod-name">XX99 MARK II HEADPHONES</p>
-                        <p className="newprod-info-text">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                        <p className="newprod-info-text">Experience natural, life-like audio and exceptional build quality made for the passionate music enthusiast.</p>
                     </div>
                 </div>
                 <div className="newprod-image"></div>
